a9b380c22943fc02a9b37eed40e165a9f0131ba5
groupeG4D <pierrehenriet@hotmail.fr>
2014-10-15 10:55:31 +0200

    commit hello world 5.2

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,8 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "Hello World!!!!";
-            echo "\nbonjour";
+            echo "Hello World!!!!bonjour";
         ?>
     </body>
 </html>
