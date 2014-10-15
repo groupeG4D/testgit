38df82966ce4a55430fc24a4bcd9c9e198b8db67
groupeG4D <pierrehenriet@hotmail.fr>
2014-10-15 10:00:41 +0200

    commit hello world 2

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "Hello World!!!!";
+            echo "Hello World!!!!"; 
+            
         ?>
     </body>
 </html>
