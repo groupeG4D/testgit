179370f89cc3c55ae5506ab0910c2820a5af5f2f
groupeG4D <pierrehenriet@hotmail.fr>
2014-10-15 11:15:25 +0200

    commit 5.8

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
             echo "Hello salepute de toi !!!!";
-            echo "\nbonjour bonjour";
+            echo "\nbonjour bonjour"; 
             
             
             
